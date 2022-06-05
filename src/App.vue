b6eec6677babdd08f62e30ce80ea895d96cab294
DESKTOP-GIJA83R\boybe <boyben001@gmail.com>
2022-06-06 01:33:17 +0800

    feat：add_meeting.vue apperance done!

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <AddMeeting />
+</template>
+
+<script>
+import AddMeeting from './components/AddMeeting.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AddMeeting
+  }
+}
+</script>
