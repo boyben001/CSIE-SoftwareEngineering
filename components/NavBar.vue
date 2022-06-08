cc32d352194afb2d5b79044b8bae7f2b0f288cd4
WJX2120 <80013069+WJX2120@users.noreply.github.com>
2022-06-08 12:11:45 +0800

    component build. v1

---
+++ components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="nav-outer-container">
+        <DeletePersonAlert :deletedname="name"/>
+    </div>
+</template>
+
+<script>
+import DeletePersonAlert from "./DeletePersonAlert.vue";
+export default {
+    name: "NavBar",
+    components: { DeletePersonAlert },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.nav-outer-container {
+    padding: 1em 1.5em;
+    background-color: #fff;
+    width: 290px;
+    border-right: 1px solid #ccc;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    overflow-y: auto;
+}
+</style>
