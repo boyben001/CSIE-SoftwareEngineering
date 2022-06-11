00e12c6e611379f8ed869f7e1f8bfa35d4173516
WU898989 <wujx89@gmail.com>
2022-06-12 02:31:45 +0800

    token 檢查完成

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,14 @@ import NewConfView from '../views/NewConfView'
 import ListConfView from '../views/ListConfView'
 import DecisionTrackView from '../views/DecisionTrackView'
 import ListMemberView from '../views/ListMemberView'
+import Cookies from 'js-cookie'
 
 const routes = [
   {
     path: '/home',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: { requireAuth: true }
   },
   {
     path: '/',
@@ -20,28 +22,60 @@ const routes = [
   {
     path: '/newConf',
     name: 'newConf',
-    component: NewConfView
+    component: NewConfView,
+    meta: { requireAuth: true }
   },
   {
     path: '/listConf',
     name: 'listConf',
-    component: ListConfView
+    component: ListConfView,
+    meta: { requireAuth: true }
   },
   {
     path: '/decisionTrack',
     name: 'decisionTrack',
-    component: DecisionTrackView
+    component: DecisionTrackView,
+    meta: { requireAuth: true }
   },
   {
     path: '/listMember',
     name: 'listMember',
-    component: ListMemberView
+    component: ListMemberView,
+    meta: { requireAuth: true }
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach(async(to, from, next) => {
+  // 看看 to 和 from 兩個 arguments 會吐回什麼訊息
+  console.log('to: ', to)
+  console.log('from: ', from)
+  
+  // 目的路由在meta上是否有設置requireAuth: true
+  if (to.meta.requireAuth) {
+    // 獲取Cookies當中的login資訊並取得token
+    const info = Cookies.get('login')
+    if (info) {
+      const token = JSON.parse(info).token
+      // 如果token不為空，且確實有這個欄位則讓路由變更
+      if (token.length > 0 || token === undefined) {
+        next()
+      } else {
+        // 未通過則導回login頁面
+        console.log('no token')
+        next({ name: 'login' })
+      }
+    } else {
+      console.log('沒有 login cookie')
+      next({ name: 'login' })
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -6,11 +6,9 @@
         <n-auto-complete v-model:value="value" :input-props="{
           autocomplete: 'disabled'
         }" :options="options" placeholder="Email" />
-        <router-link to="/home">
-          <n-button class="login_btn" tertiary type="primary">
-            登入
-          </n-button>
-        </router-link>
+        <n-button class="login_btn" tertiary type="primary" @click="handleLogin">
+          登入
+        </n-button>
 
       </div>
     </div>
@@ -20,11 +18,21 @@
 <script>
 import { defineComponent, ref, computed } from "vue";
 import { NButton } from 'naive-ui'
+import Cookies from 'js-cookie'
 
 export default defineComponent({
   components: {
     NButton
   },
+  data() {
+    return{
+      loginForm: {
+        usermail: '',
+        token: ''
+      }
+    }
+  },
+
   setup() {
     const valueRef = ref("");
     return {
@@ -39,6 +47,36 @@ export default defineComponent({
         });
       })
     };
+  },
+  methods: {
+    handleLogin() {
+      const token = 'asds32adsavrAS3Fadf5567' // token本身就是加密過的字串，隨意
+      let usermail = this.value
+      // 帳號密碼需驗證不能為空
+      if (usermail !== '') {
+        this.loginForm.token = token
+        this.loginForm.usermail = usermail
+      } else {
+        alert('帳號密碼不能為空')
+      }
+
+      var date = new Date();
+      date.setTime(date.getTime()+(10*60*1000)); //十分鐘後 cookie 會過期
+      console.log(date)
+      
+      Cookies.set('login', JSON.stringify(this.loginForm), { expires: date })
+      console.log(this.loginForm)
+      
+      // cookie當中有token被設置才能改變路由
+      if (Cookies.get('login') && this.loginForm.token) {
+        this.$router.push({name: 'home'})
+      }
+    },
+    
+    // 將Cookies清除的測試用button事件
+    removeCookie() {
+      Cookies.remove('login')
+    }
   }
 });
 </script>
